c7429c01677a801af509e0fe30b36094492020a7
siddiquekhan078698-lab <286959721+siddiquekhan078698-lab@users.noreply.github.com>
2026-07-06 15:12:31 +0500

    added backend

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -10,6 +10,9 @@ import {
 } from "@/components/ui/sidebar"
 import { PlusCircleIcon, EnvelopeIcon } from "@phosphor-icons/react"
 
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 export function NavMain({
   items,
 }: {
@@ -19,6 +22,8 @@ export function NavMain({
     icon?: React.ReactNode
   }[]
 }) {
+  const pathname = usePathname()
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -46,7 +51,7 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
+              <SidebarMenuButton tooltip={item.title} isActive={pathname === item.url} render={<Link href={item.url} />}>
                 {item.icon}
                 <span>{item.title}</span>
               </SidebarMenuButton>
